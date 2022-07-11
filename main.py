b8d218fb56e921d1301cfc81002d3e40b323f51a
moritz-reuter <89300122+moritz-reuter@users.noreply.github.com>
2022-07-11 21:05:51 +0200

    Scripts - Initialization

---
+++ main.py
@@ -0,0 +1,16 @@
+#%% Packages
+
+
+#%% Streamlit user info
+year                = 2019
+number_household    = 2
+slp_type            = 'EFH'
+lat                 = 52.52
+lon                 = 13.4050
+elec_mix            = 'DE-Mix'
+heat_tech           = 'Boiler'
+co2_price           = 300
+
+
+
+
