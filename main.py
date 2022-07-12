0e5cf8b01b4277342c8c18f4d5d60732f8a93f4d
moritz-reuter <89300122+moritz-reuter@users.noreply.github.com>
2022-07-12 17:14:29 +0200

    Smal changes --> annual demand for user input

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 #%% Packages
+import pvlib as pv
+import windpowerlib as wind
+import oemof.thermal as therm
+import demandlib as demand
+
 
 
 #%% Streamlit user info
 year                = 2019
 number_household    = 2
+annual_elec_demand  = 1500 #kWh
 slp_type            = 'EFH'
 lat                 = 52.52
 lon                 = 13.4050
@@ -12,5 +18,4 @@ heat_tech           = 'Boiler'
 co2_price           = 300
 
 
-
-
+#%% 
