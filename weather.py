8386170220dd758baef19a5d4b33aebbfd433efe
moritz-reuter <89300122+moritz-reuter@users.noreply.github.com>
2022-07-15 19:42:58 +0200

    Weather api

--- weather.py
+++ weather.py
@@ -1,2 +1,32 @@
-#%%
-# %%
+#%%from http.client import TOO_MANY_REQUESTS
+import json
+from tracemalloc import start
+import pvlib as pv
+import numpy as np
+import pandas as pd
+import requests
+
+#%% TMY Data (PVGIS)
+def tmy_data(lat, lon):
+    tmy_data_api = pv.iotools.get_pvgis_tmy(
+        latitude= lat,
+        longitude= lon,
+        map_variables= True
+    )
+    tmy = tmy_data_api[0]
+    
+    return tmy
+
+def sky_data(lat, lon, year):
+    query = {
+        'date' : str(year) + "-01-01",
+        'last_date': str(year+1) + "-01-01",
+        'lat': lat,
+        'lon': lon
+    }
+    response_API = requests.get("https://api.brightsky.dev/weather", params=query)
+    data = response_API.text
+    sky_data_api =  json.loads(data)
+    sky = pd.DataFrame.from_dict(sky_data_api["weather"][0:8760]) 
+
+    return sky
