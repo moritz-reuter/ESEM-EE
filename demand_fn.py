91a6afcccae4d7979ae2ec631ec623763d3990dc
moritz-reuter <89300122+moritz-reuter@users.noreply.github.com>
2022-07-17 15:03:47 +0200

    Functioning heat and elec demand

--- demand_fn.py
+++ demand_fn.py
@@ -1,5 +1,5 @@
 #%% Packages
-import demandlib as demand
+import demandlib.bdew as bdew
 import holidays as holi
 import helper_functions
 import os
@@ -24,7 +24,7 @@ def elec_slp(year, province, sector, ann_demand):
 
     # lp_dict = dict(zip(lp_keys,lp_demand))
 
-    pr_holidays = holi.Germany(years = [year], prov = pr_dict[province])
+    pr_holidays = [holi.Germany(years = [year], prov = x) for x in pr_keys]
     #key = sector, value = annual_value
     lp_dict = {sector: ann_demand}
     # pr_dict = dict(zip(pr_keys, pr_holidays))
@@ -35,7 +35,7 @@ def elec_slp(year, province, sector, ann_demand):
     ##
 
     #%% Electricity profiles (bdew)
-    e_slp = demand.bdew.ElecSlp(year, holidays=pr_dict[province])
+    e_slp = bdew.elec_slp.ElecSlp(year, holidays=pr_dict[province])
 
     elec_demand = e_slp.get_profile(lp_dict).asfreq('H')
 
@@ -43,6 +43,60 @@ def elec_slp(year, province, sector, ann_demand):
     return elec_demand
 
 # %% Function - HeatSlp
-def heat_slp(year, province, sector, ann_demand):
+def heat_slp(year, province, sector, ann_demand, weather):
+    heat_slp_types  = helper_functions.sheet_xl(data, "heat_slp")
+    heat_prov       = helper_functions.sheet_xl(data, "heat_prov")
 
-    return heat_demand
+    # Building Parameters (demand, share of total demand and )
+    b_keys = list(heat_slp_types.index) # --> EFH... GHD
+    b_demand = [1]*14   #GWh, since annual load data is given in GWh!
+    pr_class = heat_prov['class'].loc[province]
+    b_class = [pr_class if b in ['EFH','MFH'] else 0 for b in b_keys] 
+
+    b_lists = [b_keys, b_demand, b_class]
+
+    dict_building = {
+        k[0]: list(k[1:]) for k in zip(*b_lists)
+    }
+
+    # Province parameters
+    pr_keys = [prov for prov in heat_prov.index]
+    pr_holidays = [holi.Germany(years = [year], prov = x) for x in pr_keys]
+    pr_wind = heat_prov['windy'].values
+
+    pr_lists = [pr_keys, pr_holidays, pr_wind]
+
+    dict_provinces = {
+        k[0]: list(k[1:]) for k in zip(*pr_lists)
+    }
+
+    #%% Dataframes
+
+    ##
+    df_building = pd.DataFrame.from_dict(
+        data = dict_building,
+        orient = 'index',
+        columns = ['heat_demand', 'building_class']
+        )
+    ##
+
+    df_province = pd.DataFrame.from_dict(
+        data = dict_provinces,
+        orient = 'index',
+        columns = ['holidays', 'wind_class']
+    )
+    heat_demand_hourly = bdew.HeatBuilding(
+            pd.date_range(datetime.datetime(year, 1, 1, 0), periods=8760, freq="H"), 
+            holidays=df_province['holidays'].loc[province],      #dict province
+            temperature=weather['temp_air'],    #series csv read in
+            shlp_type=sector,  #database building_type --> join?
+            building_class= int(df_building['building_class'].loc[sector]), #dict province --> always 0!
+            wind_class= int(df_province['wind_class'].loc[province]),  #dict province
+            annual_heat_demand=ann_demand,
+            ww_incl=True,   #IMMER TRUE!
+            name=sector,
+        ).get_bdew_profile()
+
+    # regions = config.index
+
+    return heat_demand_hourly
--- main.py
+++ main.py
@@ -2,8 +2,9 @@
 import pvlib as pv
 import windpowerlib as wind
 import oemof.thermal as therm
-import demandlib as demand
+# import demandlib as demand
 import demand_fn
+import weather_fn
 
 
 #%% Streamlit user info
@@ -11,7 +12,8 @@ year                = 2017
 number_household    = 2
 annual_elec_demand  = 1500 #kWh
 annual_heat_demand  = 15000 #kWh
-slp_type            = 'EFH'
+slp_type_heat       = 'EFH'
+slp_type_elec       = 'h0' # --> write function to determine elec_slp based on heat_slp 
 lat                 = 52.52
 lon                 = 13.4050
 elec_mix            = 'DE-Mix'
@@ -19,10 +21,14 @@ heat_tech           = 'Boiler'
 co2_price           = 300
 province            = 'SH'
 
+#%% Weather
+weather_hourly = weather_fn.tmy_data(lat, lon)
+
 #%% Demand
 
 ### Electrical load profile ###
-elec_demand_hourly = demand_fn.elec_slp(year,province, slp_type, annual_elec_demand)
+elec_demand_hourly = demand_fn.elec_slp(year,province, sector = slp_type_elec, ann_demand= annual_elec_demand)
 
-heat_demand_hourly = demand_fn.heat_slp(year, province, slp_type, annual_heat_demand)
-# %%
+### Heat load profile ###
+heat_demand_hourly = demand_fn.heat_slp(year, province, slp_type_heat, annual_heat_demand, weather = weather_hourly)
+# %% 
