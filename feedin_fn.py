7aa3b5f5c08b3dae4adad6983d14081c1d1f774b
moritz-reuter <89300122+moritz-reuter@users.noreply.github.com>
2022-07-19 23:03:41 +0200

    Functioning PV (scaled by area)

--- feedin_fn.py
+++ feedin_fn.py
@@ -1,5 +1,42 @@
 #%% Packages
+import pvlib as pv
+import windpowerlib as wind
 
+from pvlib.temperature import TEMPERATURE_MODEL_PARAMETERS
+from feedinlib import Photovoltaic
+from feedinlib import get_power_plant_data
+#%% PV-Feedin
 
+def pv_elec(weather, module, inverter, lat, lon):
 
-# %% 
+    azimuth = 180
+    tilt = 37.5
+    albedo = 0.2
+    scaling  = "area"
+    mode = "ac"
+
+    temperature_model_parameters = TEMPERATURE_MODEL_PARAMETERS['sapm']['open_rack_glass_glass']
+
+    sys_data = {
+            'module_name': module,       # module name as in database
+            'inverter_name': inverter,   # inverter name as in database
+            'azimuth': azimuth,              # Azimuth angle of the module surface (South=180).
+            'tilt': tilt,                  # Surface tilt angle in decimal degrees. The tilt angle is defined as degrees from horizontal
+            'albedo': albedo,
+            'temperature_model_parameters': temperature_model_parameters}               # The ground albedo.
+
+    pv_system = Photovoltaic(**sys_data)
+
+    pv_feedin = pv_system.feedin(
+            weather=weather,
+            location=(lat, lon),
+            scaling= scaling, # can be scaled by 'area' or 'peak_power'
+            mode = 'dc' # mode is default 'ac' but can be set to 'dc' --> dc circumvents inverter losses!
+            ).reset_index(level=None,drop=True)
+
+    
+    return pv_feedin
+
+# %%
+
+# %%
--- main.py
+++ main.py
@@ -5,24 +5,42 @@ import oemof.thermal as therm
 # import demandlib as demand
 import demand_fn
 import weather_fn
+import helper_functions
+import feedin_fn
 
+data = 'data.xlsx'
 
 #%% Streamlit user info
 year                = 2017
 number_household    = 2
-annual_elec_demand  = 1500 #kWh
+annual_elec_demand  = 1500  #kWh
 annual_heat_demand  = 15000 #kWh
 slp_type_heat       = 'EFH'
-slp_type_elec       = 'h0' # --> write function to determine elec_slp based on heat_slp 
+slp_type_elec       = 'h0'  # --> write function to determine elec_slp based on heat_slp 
 lat                 = 52.52
 lon                 = 13.4050
 elec_mix            = 'DE-Mix'
 heat_tech           = 'Boiler'
 co2_price           = 300
 province            = 'SH'
+pv_area             = 10
 
 #%% Weather
 weather_hourly = weather_fn.tmy_data(lat, lon)
+
+#%% Technology Data
+tech_data   = helper_functions.sheet_xl(data, "tech")
+
+#%% Feed-in
+
+### PV ###
+module              = "Silevo_Triex_U300_Black__2014_"
+inverter            = "ABB__MICRO_0_3_I_OUTD_US_240__240V_"
+pv_elec_per_m2      = feedin_fn.pv_elec(weather_hourly, module, inverter, lat, lon)
+pv_elec             = pv_elec_per_m2*pv_area
+
+### Wind ###
+
 
 #%% Demand
 
@@ -31,4 +49,5 @@ elec_demand_hourly = demand_fn.elec_slp(year,province, sector = slp_type_elec, a
 
 ### Heat load profile ###
 heat_demand_hourly = demand_fn.heat_slp(year, province, slp_type_heat, annual_heat_demand, weather = weather_hourly)
-# %% 
+#%% CO2 - Factors
+
