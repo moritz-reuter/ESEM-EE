8766fdc82125a3cfaa909aeb19befc4872efed97
moritz-reuter <89300122+moritz-reuter@users.noreply.github.com>
2022-08-14 22:42:16 +0200

    Update main_submit.py

--- main_submit.py
+++ main_submit.py
@@ -1,7 +1,4 @@
 #%% Packages
-from os import stat_result
-from sqlite3 import SQLITE_ALTER_TABLE
-from nose import collector
 import streamlit as st
 import pvlib as pv
 import windpowerlib as wind
@@ -44,11 +41,16 @@ def submit(submission):
     hub_height              = submission['hub_height']
 
     ###############################################
+    ###############################################
     co2_price = calc.co2_price(co2_price_sim, data)
 
     #%% Weather
-    weather_hourly = weather_fn.tmy_data(lat, lon)
+    weather_hourly          = weather_fn.tmy_data(lat, lon)
+    # wind_hourly             = weather_hourly[['wind_speed', 'pressure']].rename_axis(None, axis=0)
+    # wind_hourly.columns     = pd.MultiIndex.from_product([wind_hourly.columns, [hub_height]])
 
+    # pv_hourly = weather_fn.era5_weather(lat, lon, year, 'pvlib')
+    # wind_hourly = weather_fn.era5_weather(lat, lon, year, 'windpowerlib')
 
     #%%#########    Demand      ###########################
 
@@ -68,11 +70,16 @@ def submit(submission):
     ### PV [kWh] ###
     module              = "Silevo_Triex_U300_Black__2014_"
     inverter            = "ABB__MICRO_0_3_I_OUTD_US_240__240V_"
-    pv_elec_per_m2      = feedin_fn.pv_elec(weather_hourly, module, inverter, lat, lon)
-    pv_elec             = pd.DataFrame((pv_elec_per_m2*pv_area) / 1000).set_index(time_index_year)
+    pv_elec_watt_per_m2 = feedin_fn.pv_elec(weather_hourly, module, inverter, lat, lon)
+    pv_elec             = pd.DataFrame((pv_elec_watt_per_m2*pv_area) / 1000).set_index(time_index_year)
 
-    ### Wind [kWh] ###
-    wind_elec           = feedin_fn.wind_elec(weather_hourly, hub_height, data, lat, lon)
+    #%%
+    ### Wind [kWh] ### --> BUG
+    # wind_weather = weather_hourly
+    # wind_weather.index.name = None
+    # wind_elec_watt      = feedin_fn.wind_elec(wind_hourly, hub_height, data, lat, lon)
+    # wind_elec           = pd.DataFrame(wind_elec_watt).set_index(time_index_year)
+    wind_elec           = pd.DataFrame([0]*len(pv_elec), index= time_index_year)
 
     ##########      Heat        #################################
     #%% Solar thermal - Heat feedin
@@ -90,9 +97,14 @@ def submit(submission):
 
     heat_demand_renewable = heat_demand_hourly - st_feedin['collectors_heat']
 
+    '''
+    heat_pump_el calculates the (additional) hourly electricity demand stemming from the use of a heat pump for heating.
+    Units (and dimensions) are derived from the input paramater heat_demand_renewable, which calculates the excess heat_demand after factoring 
+    in the heat energy that is provided through a solar thermal (roof) 
+    '''
     heat_pump_el, cop = thermal_fn.heat_pump_el(T_in, T_out, heat_demand_renewable, quality_grade_hp)
 
-    jaz = heat_demand_hourly.sum() / heat_pump_el.sum()
+    # jaz = heat_demand_hourly.sum() / heat_pump_el.sum()
 
 
     #%% ###### CO2 emissions ##############
@@ -105,30 +117,32 @@ def submit(submission):
     ####
     new_elec_demand = heat_pump_el + elec_demand_hourly
 
-    renewable_feedin = [pv_elec['p_mp'], wind_elec[0]]
+    renewable_feedin_dict = {'PV': pv_elec[0],
+                        'Wind': wind_elec[0]
+                        }
+    renewable_feedin = pd.DataFrame.from_dict(renewable_feedin_dict)
 
     ren_tech_co2 = []
     for ren_tech in renewable_elec:
-        for j in range(len(renewable_feedin)):
-            ren_tech_co2.append(calc.co2_kwh(ren_tech, tech_data, renewable_feedin[j]) /1000)
+        ren_tech_co2.append(calc.co2_kwh(ren_tech, tech_data, renewable_feedin_dict[ren_tech]) /1000)
 
 
-    residual_load = new_elec_demand - sum(renewable_feedin)
+    residual_load = new_elec_demand - renewable_feedin.sum(axis = 1)
 
     co2_grid = calc.co2_kwh(elec_mix, tech_data, residual_load) / 1000
 
     co2_sum_new = co2_grid + sum(ren_tech_co2)
-
-    co2_new_elec = pd.DataFrame(co2_sum_new).set_index(new_elec_demand.index)
+    #%%
+    co2_new_elec = pd.DataFrame(co2_sum_new).set_index(time_index_year)
 
     ##%% Gas 
     co2_old_gas = calc.co2_kwh(heat_tech, tech_data, heat_demand_hourly) / 1000
 
 
     #### --> adapt to necessary gas demand...
-    new_gas_demand = pd.DataFrame([0]*len(weather_hourly)).set_index(new_elec_demand.index)
+    new_gas_demand = pd.DataFrame([0]*len(weather_hourly)).set_index(time_index_year)
     ####
-    co2_new_gas = pd.DataFrame([0]*len(weather_hourly)).set_index(new_elec_demand.index)
+    co2_new_gas = pd.DataFrame([0]*len(weather_hourly)).set_index(time_index_year)
 
     #%% ############## Compensation ##########
     old_co2 = co2_old_gas + co2_old_elec
@@ -159,8 +173,5 @@ def submit(submission):
 
     total_diff = np.subtract(old_price_total, new_price_total)
 
-    ##### OUTPUT #####
-    df_calculation = pd.DataFrame()
-
-    return df_calculation
+    return comp.sum(), total_diff.sum(), price_diff.sum()
 # %%
